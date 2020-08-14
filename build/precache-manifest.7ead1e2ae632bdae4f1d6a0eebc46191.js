4494deb63fc93a23d5ba2f6d924742648268a2d1
Sushree P Mohanty <60944543+SushreeP@users.noreply.github.com>
2020-08-14 22:08:57 +0100

    Upload build directory

---
+++ build/precache-manifest.7ead1e2ae632bdae4f1d6a0eebc46191.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d87d788312f613b1d4cec8bb81118bbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd1479756ed1f766dfc",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1b73f41482028500f3bf",
+    "url": "/static/css/main.78068775.chunk.css"
+  },
+  {
+    "revision": "5bd1479756ed1f766dfc",
+    "url": "/static/js/2.d58a1064.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d58a1064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b73f41482028500f3bf",
+    "url": "/static/js/main.950f718c.chunk.js"
+  },
+  {
+    "revision": "48e9041a6219056d1d11",
+    "url": "/static/js/runtime-main.a399ba81.js"
+  },
+  {
+    "revision": "13a4504129372394740f868ab67270a1",
+    "url": "/static/media/person-1.13a45041.jpg"
+  },
+  {
+    "revision": "fbadb3c46118638dc98f071f7e9ef659",
+    "url": "/static/media/person-2.fbadb3c4.jpg"
+  },
+  {
+    "revision": "a9a22c39a5ce1f0534b9ebd14dc3fdd6",
+    "url": "/static/media/person-3.a9a22c39.jpg"
+  }
+]);
